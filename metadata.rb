1e5a9ebf9c27a271047c7eadd33705371e18be63
elliotkendallUCSF <elliot.kendall@ucsf.edu>
2013-07-26 09:58:53 -0700

    Switch from JSON to Ruby metadata

---
+++ metadata.rb
@@ -0,0 +1,11 @@
+maintainer        "Elliot Kendall"
+maintainer_email  "elliot.kendall@ucsf.edu"
+license           "Apache 2.0"
+description       "Installs Shibboleth IdP"
+version           "0.1.1"
+
+recipe "shibboleth_idp", "Installs Shibboleth IdP"
+
+%w{ centos }.each do |os|
+  supports os
+end
