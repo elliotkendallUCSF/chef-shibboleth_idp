c1e787f327c21b1440a051fc472dfe29f7776d79
elliotkendallUCSF <elliot.kendall@ucsf.edu>
2013-08-06 15:11:40 -0700

    Bump version to 0.1.2

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ maintainer        "Elliot Kendall"
 maintainer_email  "elliot.kendall@ucsf.edu"
 license           "Apache 2.0"
 description       "Installs Shibboleth IdP"
-version           "0.1.1"
+version           "0.1.2"
 
 recipe "shibboleth_idp", "Installs Shibboleth IdP"
 
